bdc056de0ea43cd17c84bcda52d3812fec66529e
Florent <grenaille.florent85@gmail.com>
2020-04-15 19:45:19 +0200

    Code review

--- acserver/views.py
+++ acserver/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse, HttpResponse, FileResponse
+from django.http import JsonResponse, FileResponse
 from .models import Server
 
 
@@ -13,5 +13,4 @@ def download(request, id_server):
     file_path = server.path_upload + "AC_NFD_Pack.zip"
     zip_file = open(file_path, 'rb')
     res = FileResponse(zip_file, as_attachment=True, content_type="application/zip")
-
     return res
