a586f99dd6a8e74b3bd1b9a3e66bc198b99a7a7f
Florent <grenaille.florent85@gmail.com>
2020-04-29 11:46:24 +0200

    Update .gitignore and clean production files

--- needfordrifting/settings/staticfiles/manageserver/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	
-	setTimeout(function(){
-        $('body').addClass('loaded');
-        $('.navbar').fadeIn('slow', function(el) {
-        });
-        $('#loader').remove();
-    }, 2000);
-});
