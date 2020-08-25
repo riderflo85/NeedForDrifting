f9e80c7fa3e67a54940f740c41182f7e4290efe3
Florent <grenaille.florent85@gmail.com>
2020-08-25 17:32:12 +0200

    Change the data type in the api endpoint

--- api/utils.py
+++ api/utils.py
@@ -3,16 +3,16 @@ def servers_to_json(servers):
     Convert the QuerySet object to the JSON object for sending to the mobile app.
     """
 
-    dict_servers = {}
+    dict_servers = []
 
     count = 1
     for server in servers:
-        dict_servers[f"server{count}"] = {
+        dict_servers.append({
             'id': str(server.id),
             'name': server.name,
             'status': server.status,
             'track': server.track.name
-        }
+        })
         count = count + 1
 
     return dict_servers
--- needfordrifting/settings/development.py
+++ needfordrifting/settings/development.py
@@ -5,7 +5,7 @@ import os
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.19']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
