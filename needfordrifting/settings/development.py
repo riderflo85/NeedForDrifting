63fc60528aa28f93652a2f514775f4ff4dc7b5ec
Florent (Mac) <grenaille.florent85@gmail.com>
2020-09-14 11:44:46 +0200

    Update development.py

--- needfordrifting/settings/development.py
+++ needfordrifting/settings/development.py
@@ -5,7 +5,7 @@ import os
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = ['192.168.1.19']
+ALLOWED_HOSTS = ['192.168.1.19', '192.168.1.47', '172.20.10.13']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
